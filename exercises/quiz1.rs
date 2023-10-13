8a0f71bdf4c60452b38e79526b6a621fccc74cd3
yrpang <yrpang@outlook.com>
2023-10-13 15:57:43 +0800

    finish 24

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,10 +13,12 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
 
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples(num: u32) -> u32 {
+    let prize = if num > 40 { num * 1 } else { num * 2 };
+    prize
+}
 
 // Don't modify this function!
 #[test]
