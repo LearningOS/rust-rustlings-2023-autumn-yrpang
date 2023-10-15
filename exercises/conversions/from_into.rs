ed796d67d4598a4f7546f1e21c3c145e77eb0180
yrpang <yrpang@outlook.com>
2023-10-15 23:05:07 +0800

    finished

--- exercises/conversions/from_into.rs
+++ exercises/conversions/from_into.rs
@@ -40,10 +40,31 @@ impl Default for Person {
 // If while parsing the age, something goes wrong, then return the default of
 // Person Otherwise, then return an instantiated Person object with the results
 
-// I AM NOT DONE
 
 impl From<&str> for Person {
     fn from(s: &str) -> Person {
+        if s.len() == 0 {
+            Person::default()
+        } else {
+            let info: Vec<&str> = s.split(",").collect();
+            if info.len() != 2 {
+                return Person::default();
+            }
+
+            let name = info[0];
+            if name.len() == 0 {
+                Person::default()
+            } else {
+                if let Ok(age) = info[1].parse::<usize>() {
+                    Person {
+                        name: String::from(name),
+                        age,
+                    }
+                } else {
+                    Person::default()
+                }
+            }
+        }
     }
 }
 
--- exercises/conversions/from_str.rs
+++ exercises/conversions/from_str.rs
@@ -31,7 +31,6 @@ enum ParsePersonError {
     ParseInt(ParseIntError),
 }
 
-// I AM NOT DONE
 
 // Steps:
 // 1. If the length of the provided string is 0, an error should be returned
@@ -52,6 +51,25 @@ enum ParsePersonError {
 impl FromStr for Person {
     type Err = ParsePersonError;
     fn from_str(s: &str) -> Result<Person, Self::Err> {
+        if s.len() == 0 {
+            Err(ParsePersonError::Empty)
+        } else {
+            let info: Vec<&str> = s.split(",").collect();
+            if info.len() != 2 {
+                return Err(ParsePersonError::BadLen);
+            }
+
+            let name = info[0];
+            if name.len() == 0 {
+                Err(ParsePersonError::NoName)
+            } else {
+                let age = info[1].parse::<usize>().map_err(ParsePersonError::ParseInt)?;
+                Ok(Person {
+                    name: String::from(name),
+                    age,
+                })
+            }
+        }
     }
 }
 
