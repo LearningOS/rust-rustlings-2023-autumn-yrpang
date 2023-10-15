5e0b927c25e790729c15874313df17dcb2ebe1e1
yrpang <yrpang@outlook.com>
2023-10-15 14:27:50 +0800

    76

--- exercises/iterators/iterators4.rs
+++ exercises/iterators/iterators4.rs
@@ -3,7 +3,6 @@
 // Execute `rustlings hint iterators4` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
 pub fn factorial(num: u64) -> u64 {
     // Complete this function to return the factorial of num
@@ -15,6 +14,13 @@ pub fn factorial(num: u64) -> u64 {
     // For an extra challenge, don't use:
     // - recursion
     // Execute `rustlings hint iterators4` for hints.
+    // if num <= 1 { 1 } else {
+    //     factorial(num - 1) * num
+    // }
+    let a = (1..5).into_iter().collect::<Vec<u64>>();
+    let a: Vec<u64> = (1..5).into_iter().collect();
+    println!("{:?}", a);
+    (1..num+1).into_iter().fold(1, |acc, x| acc * x)
 }
 
 #[cfg(test)]
